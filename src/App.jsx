1d1afb5dc4e31f19f0236448641e60c45521430b
sue-art <suepark0305@gmail.com>
2025-06-05 12:38:06 +1000

    Adding the NavMain block

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import DashboardPage from './pages/DashboardPage';
-import AboutPage from './pages/AboutPage';
-import LearningListPage from './pages/LearningListPage';
-import LoginPage from './pages/LoginPage';
-import './App.css'; // Assuming App.css might be used or created by Vite
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import AboutPage from "./pages/AboutPage";
+import LearningListPage from "./pages/LearningListPage";
+import LoginPage from "./pages/LoginPage";
+import "./App.css"; // Assuming App.css might be used or created by Vite
+import { NavMain } from "./components/dashboard/NavMain";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/learning-list">Learning List</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-        </nav>
-
-        <hr />
-
+        <NavMain />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
